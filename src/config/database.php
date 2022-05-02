7557950ea5a1be22db670b35f96d1a863c9caf7d
jeffreyrodriigues <jeffrey.rodriigues@gmail.com>
2022-05-02 16:25:25 -0300

    Create a getResultFromQuery

--- src/config/database.php
+++ src/config/database.php
@@ -1,8 +1,8 @@
 <?php
 
 class Database {
-    
-    public static function getConnection() {
+
+    public static function getConnection() { //conectando ao banco
         $envPath = realpath(dirname(__FILE__) . '/../env.ini'); //caminho absoluto
         $env = parse_ini_file($envPath); //Pega as informações da configuração
         $conn = new mysqli($env['host'], $env['username'], 
@@ -13,5 +13,12 @@ class Database {
         }
 
         return $conn;
+    }
+
+    public static function getResultFromQuery($sql) { //coletando informações do banco
+        $conn = self::getConnection();
+        $result = $conn->query($sql);
+        $conn->close();
+        return $result;
     }
 }
