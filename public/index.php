9825f10cb90a87a863cd7da98a3375e7361ee77b
jeffreyrodriigues <jeffrey.rodriigues@gmail.com>
2022-05-02 18:29:13 -0300

    getting user at DB

--- public/index.php
+++ public/index.php
@@ -3,9 +3,8 @@
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-$user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3er.com.br']);
-//echo $user->getSelect();
+foreach(User::get([], 'name') as $user) {
+    echo $user->name;
+    echo '<br>';
+}
 
-echo User::getSelect(['id' => 1], 'name, email');
-echo '<br>';
-echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
