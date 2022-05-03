b829ed31e945cc68b0cddde33a94bf0f4c52a55d
jeffreyrodriigues <jeffrey.rodriigues@gmail.com>
2022-05-03 15:36:56 -0300

    create a body of day_records.php

--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <span>Desenvolvido com</span>
     <span><i class="icofont-heart text-danger mx-1"></i></span>
-    <span>por J<span class="text-dark">3</span>ffr<span class="text-dark">3</span>y</span>
+    <span>por Jeffrey</span>
 </footer>
 <script src="assets/js/app.js"></script>
 </body>
