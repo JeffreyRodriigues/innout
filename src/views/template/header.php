122b5a6ce6b30dbd15774a67ea5e683599df7365
jeffreyrodriigues <jeffrey.rodriigues@gmail.com>
2022-05-03 12:11:31 -0300

    Iniciated creation Templates

---
+++ src/views/template/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/comum.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/template.css">
+    <title>Entra e sai</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="logo">
+            <i class="icofont-travelling mr-3"></i>
+            <span class="font-weight-light">Entra</span>
+            <span class="font-weight-bold mx-2"> e</span>
+            <span class="font-weight-light">Sai</span>
+            <i class="icofont-runner-alt-1 ml-3"></i>
+        </div>
+        <div class="menu-toggle mx-3">
+            <i class="icofont-navigation-menu"></i>
+        </div>
+        <div class="spacer"></div>
+    </header>
