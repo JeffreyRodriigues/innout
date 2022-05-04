f41bfe2016e2a2066325236ce88687b33ca6b6f7
jeffreyrodriigues <jeffrey.rodriigues@gmail.com>
2022-05-04 18:20:49 -0300

    Create Monthly Report

--- src/controllers/data_generator.php
+++ src/controllers/data_generator.php
@@ -1,5 +1,4 @@
 <?php
-loadModel('WorkingHours');
 
 Database::executeSQL('DELETE FROM working_hours');
 Database::executeSQL('DELETE FROM users WHERE id > 5');
@@ -57,7 +56,7 @@ function populateWorkingHours($userId, $initialDate, $regularRate, $extraRate, $
 }
 
 $lastMonth = strtotime('first day of last month');
-//populateWorkingHours(1, date('Y-m-1'), 70, 20, 10);
+populateWorkingHours(1, date('Y-m-1'), 70, 20, 10);
 populateWorkingHours(3, date('Y-m-d', $lastMonth), 20, 75, 5);
 populateWorkingHours(4, date('Y-m-d', $lastMonth), 20, 10, 70);
 
