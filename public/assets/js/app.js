5444b7d272d0bd11cc3fcc3ebb9d81e36a256cf2
jeffreyrodriigues <jeffrey.rodriigues@gmail.com>
2022-05-04 17:52:05 -0300

    Config Hours in Real time

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,3 +6,30 @@
         body.classList.toggle('hide-sidebar')
     }
 })()
+
+function activateClock() { //realizando a conversão e ativando o relógio
+    const activeClock = document.querySelector('[active-clock]')
+    if(!activeClock) return
+
+    function addOneSecond(hours, minutes, seconds) { //somando 1 segundo
+        const d = new Date()
+        d.setHours(parseInt(hours))
+        d.setMinutes(parseInt(minutes))
+        d.setSeconds(parseInt(seconds) + 1)
+    
+        const h = `${d.getHours()}`.padStart(2,0)
+        const m = `${d.getMinutes()}`.padStart(2,0)
+        const s = `${d.getSeconds()}`.padStart(2,0)
+    
+        return `${h}:${m}:${s}`
+    }
+
+    setInterval(function() {
+        // '07:27:19' => ['07', '27', '19']
+        const parts = activeClock.innerHTML.split(':')
+        activeClock.innerHTML = addOneSecond(...parts)
+    }, 1000)
+
+}
+
+activateClock()
